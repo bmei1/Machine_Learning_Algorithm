faad4b128e0b304e9ac076c09fa4b1c6cccba34d
bmei1 <meibing@gmail.com>
2018-05-14 13:31:16 -0400

    add gradient descent

--- LinearRegression.py
+++ LinearRegression.py
@@ -16,6 +16,42 @@ class LinearRegression:
 
         return self
 
+    def fit_gd(self, X_train, y_train, eta=0.01, n_iters=10000):
+
+        def J(theta, X_b, y):
+            try:
+                return np.sum((y - X_b.dot(theta)) ** 2) / len(y)
+            except:
+                return float('inf')
+        
+        def dJ(theta, X_b, y):
+            res = np.empty(len(theta))
+            res[0] = np.sum(X_b.dot(theta) - y)
+            for i in range(1, len(theta)):
+                res[i] = (X_b.dot(theta) - y).dot(X_b[:, i])
+            
+            return res * 2 / len(X_b)
+
+        def gradient_descent(X_b, y, initial_theta, eta, n_iters=10000, epsilon=1e-6):
+            theta = initial_theta
+            cur_iter = 0
+
+            while cur_iter < n_iters:
+                gradient = dJ(theta, X_b, y)
+                last_theta = theta
+                theta = theta - eta * gradient
+                if (abs(J(theta, X_b, y) - J(last_theta, X_b, y)) < epsilon):
+                    break
+                cur_iter += 1
+
+            return theta
+
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.zeros(X_b.shape[1])
+        self._theta = gradient_descent(X_b, y_train, initial_theta, eta, n_iters=10000)
+        self.interception = self._theta[0]
+        self.coefficient = self._theta[1:]
+
     def predict(self, X_predict):
         X_b = np.hstack([np.ones((len(X_predict), 1)), X_predict])
         return X_b.dot(self._theta)

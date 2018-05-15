28f034ea4968fd57bb7dd45241eaa684508d8a4f
bmei1 <meibing@gmail.com>
2018-05-15 01:07:12 -0400

    logistic regression

---
+++ LogisticRegression.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+class LogisticRegression:
+
+    def __init__(self):
+        self.interception = None
+        self.coefficient = None
+        self._theta = None
+
+    def _sigmoid(self, t):
+        return 1. / (1. + np.exp(-t))
+
+    def fit(self, X_train, y_train, eta=1e-4, n_iters=10000):
+
+        def J(theta, X_b, y):
+            y_hat = self._sigmoid(X_b.dot(theta))
+            try:
+                return - np.sum(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat)) / len(y)
+            except:
+                return float('inf')
+        
+        def dJ(theta, X_b, y):
+            return X_b.T.dot(self._sigmoid(X_b.dot(theta)) - y) / len(X_b)
+
+        def gradient_descent(X_b, y, initial_theta, eta, n_iters=10000, epsilon=1e-6):
+            theta = initial_theta
+            cur_iter = 0
+
+            while cur_iter < n_iters:
+                gradient = dJ(theta, X_b, y)
+                last_theta = theta
+                theta = theta - eta * gradient
+                if (abs(J(theta, X_b, y) - J(last_theta, X_b, y)) < epsilon):
+                    break
+                cur_iter += 1
+
+            return theta
+
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.zeros(X_b.shape[1])
+        self._theta = gradient_descent(X_b, y_train, initial_theta, eta, n_iters=10000)
+        self.interception = self._theta[0]
+        self.coefficient = self._theta[1:]
+
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.random.random(X_b.shape[1])
+        self._theta = sgd(X_b, y_train, initial_theta, n_iters, a, b)
+        self.interception = self._theta[0]
+        self.coefficient = self._theta[1:]
+
+
+    def predict_proba(self, X_predict):
+        X_b = np.hstack([np.ones((len(X_predict), 1)), X_predict])
+        return self._sigmoid(X_b.dot(self._theta))
+
+    def predict(self, X_predict):
+        proba = self.predict_proba(X_predict)
+        return np.array(proba >= 0.5, dtype='int')
